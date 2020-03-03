6a86e722878a0cde6e3826d41d80059d13351744
maxdvance <59272646+maxdvance@users.noreply.github.com>
2020-03-03 09:50:58 +0800

    Create sql_result_to_csv.py

---
+++ sql_result_to_csv.py
@@ -0,0 +1,13 @@
+import mysql.connector
+import pandas as pd
+
+mydb = mysql.connector.connect(host="host",user="user",passwd="password",database="db")
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM table"
+mycursor.execute(sql)
+sql_data = pd.DataFrame(mycursor.fetchall())
+sql_data.columns = mycursor.column_names
+mydb.close()
+
+sql_data.to_csv("table.csv",index = False)
