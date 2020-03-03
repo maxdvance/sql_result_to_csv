0ff2444d39e05e7d4568389887146e3098b9f44a
maxdvance <59272646+maxdvance@users.noreply.github.com>
2020-03-03 10:15:04 +0800

    Update sql_result_to_csv.py

--- sql_result_to_csv.py
+++ sql_result_to_csv.py
@@ -1,14 +1,18 @@
 import mysql.connector
 import pandas as pd
 
-mydb = mysql.connector.connect(host="host",user="user",passwd="password",database="db")
-mycursor = mydb.cursor()
+try:
+    mydb = mysql.connector.connect(host="host",user="user",passwd="password",database="db")
+    mycursor = mydb.cursor()
 
-sql = "SELECT * FROM table"
-mycursor.execute(sql)
-sql_data = pd.DataFrame(mycursor.fetchall())
-sql_data.columns = mycursor.column_names
-mydb.close()
+    sql = "SELECT * FROM table"
+    mycursor.execute(sql)
+    sql_data = pd.DataFrame(mycursor.fetchall())
+    sql_data.columns = mycursor.column_names
+    mydb.close()
 
-sql_data.to_csv("table.csv",index = False)
-#If False do not print fields for index names.
+    sql_data.to_csv("table.csv",index = False)
+    #If False do not print fields for index names.
+
+except mysql.connector.Error as err:
+    print("Something went wrong: {}".format(err))
